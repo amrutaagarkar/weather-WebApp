0a7e33f0b96adcfb46ac98ea9d94d4bd4f41efc2
Amruta Agarkar <amrutaagarkar357@gmail.com>
2026-05-15 19:13:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,158 +1,289 @@
-//making object of weatherapi
+// Weather API Object
 const weatherApi = {
     key: '4eb3703790b356562054106543b748b2',
     baseUrl: 'https://api.openweathermap.org/data/2.5/weather'
-}
+};
 
-//anonymous function
-//adding event listener key press of enter
+// Search Input
 let searchInputBox = document.getElementById('input-box');
+
 searchInputBox.addEventListener('keypress', (event) => {
-    if (event.keyCode == 13) {
-        // console.log(searchInputBox.value);
+
+    if (event.key === 'Enter') {
         getWeatherReport(searchInputBox.value);
-        
     }
-})
 
+});
 
-//get waether report
-
+// Fetch Weather Report
 function getWeatherReport(city) {
-    fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)  // fetch method fetching the data from  base url ...metric is used for unit in celcius......here i am appending the base url to get data by city name .  
-        .then(weather => {   //weather is from api
-            return weather.json(); // return data from api in JSON
-        }).then(showWeaterReport);  // calling showweatherreport function
+
+    fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)
+
+    .then(response => response.json())
+
+    .then(showWeatherReport)
+
+    .catch(error => {
+        console.log(error);
+        swal("Error", "Something went wrong!", "error");
+    });
 
 }
 
-//show weather report
+// Show Weather Report
+function showWeatherReport(weather) {
 
-function showWeaterReport(weather) {
-    let city_code=weather.cod;
-    if(city_code==='400'){ 
-        swal("Empty Input", "Please enter any city", "error");
+    let city_code = weather.cod;
+
+    if (city_code == 400) {
+
+        swal("Empty Input", "Please enter city name", "error");
         reset();
-    }else if(city_code==='404'){
-        swal("Bad Input", "entered city didn't matched", "warning");
-        reset();
+
     }
-    else{
 
-    // console.log(weather.cod);
-    // console.log(weather);  
-    let op = document.getElementById('weather-body');
-    op.style.display = 'block';
-    let todayDate = new Date();
-    let parent=document.getElementById('parent');
-    let weather_body = document.getElementById('weather-body');
-    weather_body.innerHTML =
-        `
-    <div class="location-deatils">
-        <div class="city" id="city">${weather.name}, ${weather.sys.country}</div>
-        <div class="date" id="date"> ${dateManage(todayDate)}</div>
-    </div>
-    <div class="weather-status">
-        <div class="temp" id="temp">${Math.round(weather.main.temp)}&deg;C </div>
-        <div class="weather" id="weather"> ${weather.weather[0].main} <i class="${getIconClass(weather.weather[0].main)}"></i>  </div>
-        <div class="min-max" id="min-max">${Math.floor(weather.main.temp_min)}&deg;C (min) / ${Math.ceil(weather.main.temp_max)}&deg;C (max) </div>
-        <div id="updated_on">Updated as of ${getTime(todayDate)}</div>
-    </div>
-    <hr>
-    <div class="day-details">
-        <div class="basic">Feels like ${weather.main.feels_like}&deg;C | Humidity ${weather.main.humidity}%  <br> Pressure ${weather.main.pressure} mb | Wind ${weather.wind.speed} KMPH</div>
-    </div>
-    `;
-    parent.append(weather_body);
-    changeBg(weather.weather[0].main);
-    reset();
+    else if (city_code == 404) {
+
+        swal("City Not Found", "Please enter correct city", "warning");
+        reset();
+
+    }
+
+    else {
+
+        let weather_body = document.getElementById('weather-body');
+
+        weather_body.style.display = "block";
+
+        let todayDate = new Date();
+
+        weather_body.innerHTML = `
+
+            <div class="location-deatils">
+
+                <div class="city">
+                    ${weather.name}, ${weather.sys.country}
+                </div>
+
+                <div class="date">
+                    ${dateManage(todayDate)}
+                </div>
+
+            </div>
+
+            <div class="weather-status">
+
+                <div class="temp">
+                    ${Math.round(weather.main.temp)}&deg;C
+                </div>
+
+                <div class="weather">
+                    ${weather.weather[0].main}
+                    <i class="${getIconClass(weather.weather[0].main)}"></i>
+                </div>
+
+                <div class="min-max">
+                    ${Math.floor(weather.main.temp_min)}&deg;C (min) /
+                    ${Math.ceil(weather.main.temp_max)}&deg;C (max)
+                </div>
+
+                <div id="updated_on">
+                    Updated as of ${getTime(todayDate)}
+                </div>
+
+            </div>
+
+            <hr>
+
+            <div class="day-details">
+
+                <div class="basic">
+                    Feels like ${Math.round(weather.main.feels_like)}&deg;C |
+                    Humidity ${weather.main.humidity}%
+                    <br>
+                    Pressure ${weather.main.pressure} mb |
+                    Wind ${weather.wind.speed} KM/H
+                </div>
+
+            </div>
+        `;
+
+        // Change Background
+        changeBg(weather.weather[0].main);
+
+        // Reset Input
+        reset();
     }
 }
 
-
-
-//making a function for the  last update current time 
-
+// Time Function
 function getTime(todayDate) {
-    let hour =addZero(todayDate.getHours());
-    let minute =addZero(todayDate.getMinutes());
+
+    let hour = addZero(todayDate.getHours());
+    let minute = addZero(todayDate.getMinutes());
+
     return `${hour}:${minute}`;
 }
 
-//date manage for return  current date
+// Date Function
 function dateManage(dateArg) {
-    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ];
+
+    let months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
 
     let year = dateArg.getFullYear();
     let month = months[dateArg.getMonth()];
     let date = dateArg.getDate();
     let day = days[dateArg.getDay()];
-    // console.log(year+" "+date+" "+day+" "+month);
-    return `${date} ${month} (${day}) , ${year}`
+
+    return `${date} ${month} (${day}), ${year}`;
 }
 
-// function for the dynamic background change  according to weather status
+// Change Background Function
 function changeBg(status) {
+
     if (status === 'Clouds') {
-        document.body.style.backgroundImage = 'url(img/clouds.jpg)';
-    } else if (status === 'Rain') {
-        document.body.style.backgroundImage = 'url(img/rainy.jpg)';
-    } else if (status === 'Clear') {
-        document.body.style.backgroundImage = 'url(img/clear.jpg)';
+
+        document.body.style.backgroundImage = "url('./img/clouds.jpg')";
+
     }
+
+    else if (status === 'Rain') {
+
+        document.body.style.backgroundImage = "url('./img/rainy.jpg')";
+
+    }
+
+    else if (status === 'Clear') {
+
+        document.body.style.backgroundImage = "url('./img/clear.jpg')";
+
+    }
+
     else if (status === 'Snow') {
-        document.body.style.backgroundImage = 'url(img/snow.jpg)';
+
+        document.body.style.backgroundImage = "url('./img/snow.jpg')";
+
     }
-    else if (status === 'Sunny') {
-        document.body.style.backgroundImage = 'url(img/sunny.jpg)';
-    } else if (status === 'Thunderstorm') {
-        document.body.style.backgroundImage = 'url(img/thunderstrom.jpg)';
-    } else if (status === 'Drizzle') {
-        document.body.style.backgroundImage = 'url(img/drizzle.jpg)';
-    } else if (status === 'Mist' || status === 'Haze' || status === 'Fog') {
-        document.body.style.backgroundImage = 'url(img/mist.jpg)';
+
+    else if (status === 'Thunderstorm') {
+
+        document.body.style.backgroundImage = "url('./img/thunderstorm.jpg')";
+
+    }
+
+    else if (status === 'Drizzle') {
+
+        document.body.style.backgroundImage = "url('./img/drizzle.jpg')";
+
+    }
+
+    else if (
+        status === 'Mist' ||
+        status === 'Haze' ||
+        status === 'Fog'
+    ) {
+
+        document.body.style.backgroundImage = "url('./img/mist.jpg')";
+
     }
 
     else {
-        document.body.style.backgroundImage = 'url(img/bg.jpg)';
+
+        document.body.style.backgroundImage = "url('./img/bg.jpg')";
+
     }
 }
 
-//making a function for the classname of icon
+// Weather Icon Function
 function getIconClass(classarg) {
+
     if (classarg === 'Rain') {
+
         return 'fas fa-cloud-showers-heavy';
-    } else if (classarg === 'Clouds') {
+
+    }
+
+    else if (classarg === 'Clouds') {
+
         return 'fas fa-cloud';
-    } else if (classarg === 'Clear') {
-        return 'fas fa-cloud-sun';
-    } else if (classarg === 'Snow') {
-        return 'fas fa-snowman';
-    } else if (classarg === 'Sunny') {
+
+    }
+
+    else if (classarg === 'Clear') {
+
         return 'fas fa-sun';
-    } else if (classarg === 'Mist') {
+
+    }
+
+    else if (classarg === 'Snow') {
+
+        return 'fas fa-snowflake';
+
+    }
+
+    else if (
+        classarg === 'Thunderstorm' ||
+        classarg === 'Drizzle'
+    ) {
+
+        return 'fas fa-bolt';
+
+    }
+
+    else if (
+        classarg === 'Mist' ||
+        classarg === 'Haze' ||
+        classarg === 'Fog'
+    ) {
+
         return 'fas fa-smog';
-    } else if (classarg === 'Thunderstorm' || classarg === 'Drizzle') {
-        return 'fas fa-thunderstorm';
-    } else {
+
+    }
+
+    else {
+
         return 'fas fa-cloud-sun';
+
     }
 }
 
+// Reset Input
 function reset() {
-    let input = document.getElementById('input-box');
-    input.value = "";
+
+    document.getElementById('input-box').value = "";
+
 }
 
-// funtion to add zero if hour and minute less than 10
+// Add Zero
 function addZero(i) {
+
     if (i < 10) {
         i = "0" + i;
     }
+
     return i;
 }
-
-
-
